7f82e0f14d3999bf260a66e502f80eacd5e9bdf6
Sergei Medvedev <50055374+JavaMedvedev@users.noreply.github.com>
2022-05-26 21:03:56 +0100

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -228,3 +228,13 @@
   });
 
 })()
+
+$(document).ready(function () {
+  $(".video-gallery").magnificPopup({
+    delegate: "a",
+    type: "iframe",
+    gallery: {
+      enabled: true
+    }
+  });
+});
