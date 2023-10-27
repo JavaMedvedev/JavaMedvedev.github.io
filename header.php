62468a6f154b428bbe64e7b86204b30a253b1792
Sergei Medvedev <50055374+JavaMedvedev@users.noreply.github.com>
2023-10-27 23:39:53 +0100

    Add files via upload

---
+++ header.php
@@ -0,0 +1,39 @@
+<header>
+<nav id="scrollspy" class="navbar navbar-light bg-light navbar-expand-lg fixed-top" data-spy="affix" data-offset-top="20">
+        <div class="container">
+           <a class="navbar-brand" href="https://javamedvedev.github.io"><img src="https://stories.nuigalway.ie/latest-press-releases/assets/logo.png" alt="" class="brand-img"></a>
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <ul class="navbar-nav ml-auto">
+                    <li class="nav-item">
+                        <a class="nav-link" href="index">Home</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="research">Research</a>
+                    </li>
+                    <div class="dropdown">
+			<ul class="dropdown-header" style="color:#a70050; font-weight: bold; font-size:15px; margin-left:0px; margin-right:-15px; margin-top:-0.5px;">Learning Technologies</ul>
+			<div class="dropdown-content" style="margin-left: 20px;">
+			  <a href="https://javamedvedev.github.io/canvas">Canvas VLE</a>
+			  <a href="https://javamedvedev.github.io/video">Blackboard, Moodle, Instructional Design, etc.</a>
+			</div>
+		    </div>
+                    <li class="nav-item">
+                        <a class="nav-link" href="software">Programming for Higher Education</a>
+                    </li>
+			<div class="dropdown">
+			  <ul class="dropdown-header"style="color:#a70050; font-weight: bold; font-size:15px; margin-left:0px; margin-top:-0.5px;">Web Technologies</ul>
+			  <div class="dropdown-content" style="margin-left: 20px;">
+			    <a href="https://javamedvedev.github.io/websites">Web Design</a>
+			    <a href="https://javamedvedev.github.io/integration">Web Data Integration</a>
+			    <a href="https://javamedvedev.github.io/design">Graphic Design</a>
+			  </div>
+			</div>
+                </ul>
+            </div>
+        </div>
+    </nav>
+</header>
